da5630628c3481ef7f395e9d0ae260951b0e9927
mavff <marcoantvff4377@gmail.com>
2024-06-14 12:23:17 -0300

    adicionando detecção de colisão

--- car.js
+++ car.js
@@ -9,14 +9,45 @@ class Car{
         this.Maxspeed=3;
         this.acceleration=0.2;
         this.atrito=0.05;
-
         this.angle=0;
+        this.damaged=false;
+
         this.sensor= new Sensors(this);
         this.controls=new Controls();
     }
     update(roadBorders){
+        if(!this.damaged){
         this.#movimentation();
+        this.polygon=this.#createPolygon();
+        this.damaged=this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
+    }
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({x:this.x-Math.sin(this.angle-alpha)*rad,
+                    y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
     }
     #movimentation(){
 
@@ -55,20 +86,52 @@ class Car{
     }   
 
 draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
+    if(this.damaged){
+        ctx.fillStyle="gray";
+    }else{
+        ctx.fillStyle="black";
+    }
         ctx.beginPath();
-        ctx.rect(-this.width/2,
-                -this.height/2,
-                this.width,
-                this.height
-                );
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
         ctx.fill();
-
-        ctx.restore();
-        
         this.sensor.draw(ctx);
     }
+}
+function polysIntersect(poly1,poly2){
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            const touch=getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+                );
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function getIntersection(A, B, C, D) {
+    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+    if (bottom != 0) {
+        const t = tTop / bottom;
+        const u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+            return {
+                x: lerp(A.x, B.x, t),
+                y: lerp(A.y, B.y, t),
+                offset: t
+            };
+        }
+    }
+
+    return null;
 }
